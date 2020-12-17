218432e52e6149448f187e8a06bcf7d941f5c1aa
BuildBetter <75692074@qq.com>
2020-12-17 19:58:30 +0800

    切割头部和尾部子视图

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,14 @@
+<div class="navbar navbar-expand-lg navbar-dark bg-dark navbar-static-top">
+    <div class="container">
+        <a class="navbar-brand" href="/" title="">Weibo App</a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target=".navbar-collapse" type="button">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item"><a class="nav-link" href="help" title="">帮助</a></li>
+                <li class="nav-item"><a class="nav-link" href="#" title="">登录</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
