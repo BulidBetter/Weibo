80f6aeaecbe0335d78d3091c957951babef53497
BuildBetter <75692074@qq.com>
2020-12-17 20:22:29 +0800

    用户注册页面

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
     <meta charset="utf-8" />
     <title>@yield('title') - Web App</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'PagesController@index')->name('pages.index');
 Route::get('help', 'PagesController@help')->name('pages.help');
 Route::get('about', 'PagesController@about')->name('pages.about');
+Route::get('register', 'UsersController@create')->name('users.create');
