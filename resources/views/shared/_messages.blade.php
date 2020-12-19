44fdf7b7fdd50237c844e368a0dfe4ca3746408f
BuildBetter <75692074@qq.com>
2020-12-19 20:23:56 +0800

    用户退出登录

--- resources/views/shared/_messages.blade.php
+++ resources/views/shared/_messages.blade.php
@@ -1,5 +1,8 @@
 @foreach ([ 'success', 'danger', 'warning', 'info' ] as $msg)
     @if ( session()->has($msg) )
-        <div class="alert alert-{{ $msg }}">{{ session()->get($msg) }}</div>
+        <div class="alert alert-{{ $msg }} alert-dismissible fade show">
+            <a class="close" data-dismiss="alert" href="javascript:void(0);" title="">&times;</a>
+            {{ session()->get($msg) }}
+        </div>
     @endif
 @endforeach
