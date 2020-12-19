7743042e3e6633c6537c234ac0254ddb8b979ff2
BuildBetter <75692074@qq.com>
2020-12-19 15:14:47 +0800

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -12,6 +12,27 @@ class UsersController extends Controller
         return view('users.create');
     }
 
+    public function store(Request $request)
+    {
+        $message = [
+            'name.required' => '昵称不能为空。',
+            'name.unique' => '该昵称已存在。',
+            'email.required' => '邮箱不能为空。',
+            'email.email' => '该邮箱不是有效的邮箱地址。',
+            'email.unique' => '该邮箱已被注册。',
+            'password.required' => '密码不能为空。',
+            'password.confirmed' => '两次密码输入不一致。'
+        ];
+
+        $this->validate($request, [
+            'name' => 'required|unique:users',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed'
+        ], $message);
+
+        return [];
+    }
+
     public function show(User $user)
     {
         return view('users.show', [ 'user' => $user ]);
